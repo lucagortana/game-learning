4298156610f5a31b58b98c77b1a5000faf10d924
Louis Manceron <louis.manceron@gmail.com>
2023-04-18 22:06:34 +0200

    wine

--- mod/wine_regression.py
+++ mod/wine_regression.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import random
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import mutual_info_regression
@@ -36,10 +38,71 @@ def generator_batch(batch_size, X, y):
     num_examples = len(X)
     indices = list(range(num_examples))
 
-    np.random.random.shuffle(indices)
+    random.shuffle(indices)
     for i in range(0, num_examples, batch_size):
         batch_indices = np.array(indices[i:min(i+batch_size,num_examples)])
         yield X[batch_indices], y[batch_indices]
 
 
+def labeled_barplot(data, feature, perc=False, n=None):
 
+    total = len(data[feature])  # length of the column
+    count = data[feature].nunique()
+    if n is None:
+        plt.figure(figsize=(count + 1, 5))
+    else:
+        plt.figure(figsize=(n + 1, 5))
+
+    plt.xticks(rotation=90, fontsize=15)
+    ax = sns.countplot(
+        data=data,
+        x=feature,
+        palette="Paired",
+        order=data[feature].value_counts().index[:n].sort_values(),
+    )
+
+    for p in ax.patches:
+        if perc == True:
+            label = "{:.1f}%".format(
+                100 * p.get_height() / total
+            )  # percentage of each class of the category
+        else:
+            label = p.get_height()  # count of each level of the category
+
+        x = p.get_x() + p.get_width() / 2  # width of the plot
+        y = p.get_height()  # height of the plot
+
+        ax.annotate(
+            label,
+            (x, y),
+            ha="center",
+            va="center",
+            size=12,
+            xytext=(0, 5),
+            textcoords="offset points",
+        )  # annotate the percentage
+
+    plt.show()  # show the plot
+
+
+def linear_regression(X, w, b): #@save
+    """The linear regression model."""
+    return np.dot(X, w) + b
+
+def initialize_params(X):
+    w = np.random.normal(0, 0.1, size=(X.shape[1], 1)).astype(np.float32)
+    b = np.zeros(1).astype(np.float32)
+    return w,b
+
+
+def mean_squared_loss(y_hat, y): #@save
+    """Squared loss."""
+    return ((1/2)*(y_hat - y.reshape(y_hat.shape))**2).mean()
+
+def mbgd(X, params, batch_size, lr=0.005): #@save
+    y_hat = linear_regression(X, params[0], params[1])
+    error = (y_hat - y.reshape(y_hat.shape))
+    w_derivative = (np.dot(X.T,error)).sum()
+    params[0] = params[0] - ((lr/batch_size)*w_derivative)
+    params[1] = params[1] - ((lr/batch_size)*error.sum())
+    return params[0],params[1]
